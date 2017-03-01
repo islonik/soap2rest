d34117a0ffa8919c2a5c706e4ecd708b50a7eb2d
Nikita <nikita.lipatov@tele2.com>
2017-03-01 13:45:52 +0200

    add interceptor ExecutionTime

---
+++ utils/src/main/java/org/javaee/soap2rest/utils/interceptors/ExecutionTime.java
@@ -0,0 +1,33 @@
+package org.javaee.soap2rest.utils.interceptors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.InvocationContext;
+import java.lang.reflect.Method;
+
+/**
+ * Created by nikilipa on 3/1/17.
+ */
+public class ExecutionTime {
+
+    private static final Logger log = LoggerFactory.getLogger(ExecutionTime.class);
+
+    @AroundInvoke
+    public Object performance(InvocationContext invocationContext)
+            throws Exception {
+        long start = System.currentTimeMillis();
+
+        Method method = invocationContext.getMethod();
+
+        Object object = invocationContext.proceed();
+
+        long end = System.currentTimeMillis();
+
+        log.info("Execution time is {} for the method '{}'", (end - start), method);
+
+        return object;
+    }
+    
+}
